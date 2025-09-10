3e6829d222f16138903ff24fab18db917a9aa6b5
Dage McNitt <mcnitd@rpi.edu>
2025-09-10 15:12:39 -0400

    Different Changes

--- BooleanDemo.java
+++ BooleanDemo.java
@@ -7,11 +7,8 @@ public class BooleanDemo {
 	public static void main(String[] args) {
 		Boolean b1 = new Boolean(true);
 		Boolean b2 = new Boolean(true);
-		Boolean b3 = new Boolean(true);
-		Boolean b4 = new Boolean(true);
 		
 		System.out.println("This is a change.");
-		System.out.println("This is another change.");
 	}
 
 }
